30d10834d8d1f4c4ddba0517e7181552873bdb61
powell956 <powell956@gmail.com>
2018-12-31 07:42:00 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,14 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+
+  array.each do |el|
+    if new_hash.keys.include?(el)
+      new_hash[el] += 1
+    else
+      
+    end 
+  end 
+  
+  new_hash
 end
  
