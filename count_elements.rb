f0ec0409dd8b4b0f3b3ff2dbf7fd8cdf870b497e
powell956 <powell956@gmail.com>
2018-12-31 07:42:23 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+
+  array.each do |el|
+    if new_hash.keys.include?(el)
+      new_hash[el] += 1
+    else
+      new_hash[el] = 1
+    end
+  end
+
+  new_hash
 end
- 
